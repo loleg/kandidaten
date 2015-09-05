ecbf658fdc899cafc5b7fe582f4d781bba38224b
Oleg Lavrovsky <oleg@utou.ch>
2015-09-05 14:33:19 +0200

    ExpandQuote

--- web/app/scripts/main.js
+++ web/app/scripts/main.js
@@ -1,20 +1,15 @@
 // jshint ignore: start
 
-function loadFeatured() {
-  data = {
-    'featured': [
-      {
-        'name': 'Bob'
-      },
-      {
-        'name': 'Jim'
-      }
-    ]
-  };
+function loadFeatured(data) {
+  console.log(data);
   var template = $('#template-featured').html();
   Mustache.parse(template);
-  var rendered = Mustache.render(template, data);
+  var rendered = Mustache.render(template, {'featured': data});
   $('#target-featured').html(rendered);
 }
 
-loadFeatured();
+function expandQuote(e) {
+  return false;
+}
+
+$.get('/api/comment/', loadFeatured);
