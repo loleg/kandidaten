108c047019eec2381717ae196de95ffd31079156
Oleg Lavrovsky <oleg@utou.ch>
2015-09-05 13:28:53 +0200

    Redirect to app

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 
 from flask_peewee.db import Database
 
@@ -6,3 +6,7 @@ app = Flask(__name__)
 app.config.from_object('config.Configuration')
 
 db = Database(app)
+
+@app.route('/')
+def index():
+    return redirect('/app')
